931a569cc54c554cc820bc62fbdec2a09ae04e57
GASOMINALI Olivier <ogasominali@gmail.com>
2022-06-01 10:29:23 +0200

    Add Events

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import react from 'react';
-
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './index.css';
 import Calculator from './components/calculator';
+import Home from './components/Home';
+import Quote from './components/Quote';
+import Header from './components/Header';
 
-class App extends react.Component {
-  render() {
-    return <Calculator />;
-  }
-}
+const App = () => (
+  <>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/Quote" element={<Quote />} />
+    </Routes>
+  </>
+);
 
 export default App;
