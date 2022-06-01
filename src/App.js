474c088d49e925e3a0e2be84661de16187f03ec5
GASOMINALI Olivier <ogasominali@gmail.com>
2022-06-01 09:43:09 +0200

    Update the changes

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+import Calculator from './components/calculator';
+
+class App extends react.Component {
+  render() {
+    return <Calculator />;
+  }
+}
+
+export default App;
